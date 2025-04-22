3cea8b09f104a65d2b4800987c7da94c18aa3cc6
citronoiseau <avroraka2004@gmail.com>
2025-04-22 19:16:11 +0000

    added sad picture for lost

--- src/components/Dialog.jsx
+++ src/components/Dialog.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useRef } from "react";
 import pokemon from "/pokemon.png";
+import sad from "/sad.png";
 
 export default function Dialog({ score, isWin, restartGame, visible }) {
   const dialogRef = useRef();
@@ -14,9 +15,12 @@ export default function Dialog({ score, isWin, restartGame, visible }) {
     <dialog ref={dialogRef}>
       <div className="dialog-container">
         <h1>{isWin ? "You won!" : "You lost!"}</h1>
-        <div class="image-container">
-          {" "}
-          <img src={pokemon} />
+        <div className="image-container">
+          {isWin ? (
+            <img src={pokemon} alt="Pokemon" />
+          ) : (
+            <img src={sad} alt="Sad face" />
+          )}
         </div>
         <div className="dialog-score">Score: {score}</div>
         <button
